f54493a307692d7834291f4c934d60ed9ae46a02
milk921139 <46059120+milk921139@users.noreply.github.com>
2022-10-28 20:35:11 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -145,3 +145,8 @@ function convertToCelsius(event) {
   temperatureElement.innerHTML = 19;
 }
 
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", convertToFahrenheit);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", convertToCelsius);
