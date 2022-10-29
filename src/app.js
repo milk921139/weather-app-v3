78fe1b0e140990385c4cd7ae7c2ec6b95db1b9ef
milk921139 <46059120+milk921139@users.noreply.github.com>
2022-10-28 20:40:27 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -133,17 +133,7 @@ form.addEventListener("submit", handleSubmit);
 
 search("New York");
 
-function convertToFahrenheit(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = 66;
-}
 
-function convertToCelsius(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = 19;
-}
 
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", convertToFahrenheit);
