d050e291c673d1098a3b146f169f7b40eccae903
milk921139 <46059120+milk921139@users.noreply.github.com>
2022-10-28 19:20:14 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -111,6 +111,15 @@ function handleSubmit(event) {
   search(cityInputElement.value);
 }
 
+function convertToF(celsius) {
+  // make the given fahrenheit variable equal the given celsius value
+  // multiply the given celsius value by 9/5 then add 32
+  let fahrenheit = celsius * 9/5 + 32
+  // return the variable fahrenheit as the answer
+  return fahrenheit;
+}
+
+convertToF(30);
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
